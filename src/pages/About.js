7a0f3f7fcf6031d25853a09d2322c8b29b8fde9c
DenZaiyy <denzaiyy@outlook.fr>
2023-02-11 02:33:46 +0100

    first commit for my first react app with an api

---
+++ src/pages/About.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+                voluptas qui dolorum beatae, doloremque, ea harum, atque esse
+                rem ab cumque iusto earum quos ratione. Quod dolor aspernatur
+                quam voluptas magni dolorem totam? Voluptatem, unde ex. Dolore
+                facere saepe quod! Eligendi officiis dolore nam dolorem ea!
+                Quam, officia quas! Reiciendis ex maiores quidem ipsum sint
+                pariatur temporibus ad cumque totam, omnis perferendis rerum,
+                sed ab aspernatur, doloribus nam. Maxime similique fuga, tenetur
+                velit possimus harum voluptatum, nemo nihil consequuntur
+                blanditiis corrupti voluptatibus dolorum? Ea, doloremque modi
+                iure debitis sunt incidunt quibusdam. Cupiditate tenetur velit
+                dignissimos aliquam dolorem vel qui iure.
+            </p>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+                voluptas qui dolorum beatae, doloremque, ea harum, atque esse
+                rem ab cumque iusto earum quos ratione. Quod dolor aspernatur
+                quam voluptas magni dolorem totam? Voluptatem, unde ex. Dolore
+                facere saepe quod! Eligendi officiis dolore nam dolorem ea!
+                Quam, officia quas! Reiciendis ex maiores quidem ipsum sint
+                pariatur temporibus ad cumque totam, omnis perferendis rerum,
+                sed ab aspernatur, doloribus nam. Maxime similique fuga, tenetur
+                velit possimus harum voluptatum, nemo nihil consequuntur
+                blanditiis corrupti voluptatibus dolorum? Ea, doloremque modi
+                iure debitis sunt incidunt quibusdam. Cupiditate tenetur velit
+                dignissimos aliquam dolorem vel qui iure.
+            </p>
+        </div>
+    );
+};
+
+export default About;
